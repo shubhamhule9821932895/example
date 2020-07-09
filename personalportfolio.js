b94bdc1c3b4c91e12eca59657c189379f53a379e
shubhamhule9821932895 <63298816+shubhamhule9821932895@users.noreply.github.com>
2020-07-09 14:35:42 +0530

    Add files via upload

--- personalportfolio.js
+++ personalportfolio.js
@@ -1,9 +1,25 @@
 var navSlide = function () {
-  /*var burger = document.querySelector(".burger");
-  var nav = document.querySelector(".nav-links");*/
+    let burger = document.querySelector(".burger");
+    let nav = document.querySelector(".nav-links");
+    let navLinks = document.querySelectorAll(".nav-links li");
+  
+    burger.addEventListener("click", function () {
+        nav.classList.toggle("nav-active");             // trnaslating the navbar 
 
-  burger.addEventListener("click", function () {
-    nav.classList.toggle("nav-active");
-  });
-};
-navSlide();
+        navLinks.forEach((links,index) => {             // delaying and animating the links
+            if(links.style.animation){
+                links.style.animation = '';
+            }
+            else{
+                links.style.animation = `navLinkFade 0.5s ease forwards ${index / 6 + 0.4}s`;
+            }
+        })
+
+        burger.classList.toggle('toggle');
+    });
+
+
+
+    
+  };
+  navSlide();
