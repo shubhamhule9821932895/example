960857369c90bffff7cb11600a970b9c67a720d2
shubhamhule9821932895 <63298816+shubhamhule9821932895@users.noreply.github.com>
2020-07-16 01:17:22 +0530

    Add files via upload

--- personalportfolio.js
+++ personalportfolio.js
@@ -39,4 +39,28 @@ var swiper = new Swiper('.swiper-container', {
   });
 
   //==================================================================
+
+
+  // Porfolio isotope and filter
+  $(window).on('load', function() {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item',
+      layoutMode: 'fitRows'
+    });
+
+    $('#portfolio-flters li').on('click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+    });
+
+    // Initiate venobox (lightbox feature used in portofilo)
+    $(document).ready(function() {
+      $('.venobox').venobox();
+    });
+  });
+
   navSlide();
